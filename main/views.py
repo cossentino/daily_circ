4bf8af450f4d18870db674136d99940166fe33d6
Ian Cossentino <icossentino@gmail.com>
2021-06-23 14:31:18 -0400

    Add basic styling to headline cards

--- main/views.py
+++ main/views.py
@@ -9,8 +9,12 @@ from .services.scraper import Scraper
 def news_view(request):
   reuters_scraper = Scraper('https://www.reuters.com')
   reuters_article = reuters_scraper.random_choice()
+  if len(reuters_article[1]) > 300:
+    reuters_article[1] = reuters_article[1][:300]
   nyer_scraper = Scraper('https://www.newyorker.com')
   nyer_article = nyer_scraper.random_choice()
+  if len(nyer_article[1]) > 300:
+    nyer_article[1] = nyer_article[1][:300]
   return render(
   request,
   'main/news.html',
@@ -20,7 +24,9 @@ def news_view(request):
     'article_1_info': reuters_article[0],
     'article_1_preview': reuters_article[1],
     'article_2_info': nyer_article[0],
-    'article_2_preview': nyer_article[1]
+    'article_2_preview': nyer_article[1],
+    'article_1_sitename': reuters_scraper.site_name,
+    'article_2_sitename': nyer_scraper.site_name,
   })
 
 
