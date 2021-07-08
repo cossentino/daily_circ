46f77d5046405bf7bc6191fb53ebac867f1b3bc2
Ian Cossentino <icossentino@gmail.com>
2021-07-08 17:44:15 -0400

    add function to return topmost circle element

--- main/static/main/main.js
+++ main/static/main/main.js
@@ -8,27 +8,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   document.getElementById('start').addEventListener('click', () => {
-    circ.rotate = true
-    circ.rotateStart()
-    toggleCardMask()
+    circ.rotateOn = true
+    circ.rotate()
+    toggleCardMask(circ)
   })
 
   document.getElementById('stop').addEventListener('click', () => {
-    circ.rotate = false
-    toggleCardMask(false)
+    circ.rotateOn = false
+    toggleCardMask(circ, false)
   })
 
 })
 
-function toggleCardMask(start=true) {
+function toggleCardMask(circ, start=true) {
   const masks = document.getElementsByClassName('card-mask')
   for (mask of masks) {
     if (start === true) {
       mask.style.display = "flex"
-    } else {
-      mask.style.display = "none"
     }
   }
+  if (start === false) {
+    const el = circ.getTopmostElement()
+    const mask = el.querySelector('div.card-mask')
+    mask.style.display = "none"
+  }
+
 }
 
 
@@ -48,7 +52,7 @@ class Circle {
     this.centerX = centerX
     this.centerY = centerY
     this.objects = []
-    this.rotate = false
+    this.rotateOn = false
   }
 
   center() {
@@ -71,11 +75,13 @@ class Circle {
     return localTheta + globalTheta
   }
 
-  async rotateStart() {
+  // All objects on the circle stored in objects property in form [dom_element, theta]. On 5ms delay, theta is increased by 1 radian
+  // until circle's rotate property is turned off. 
+  async rotate() {
     let globalTheta = 0
     const container = document.getElementsByClassName('headlines-container')[0]
     const containerCenter = elemCenter(container)
-    while (this.rotate === true) {
+    while (this.rotateOn === true) {
       for (let card of this.objects) {
         const newY = containerCenter[1] - this.radius * (Math.sin(this.myTheta(card[1], globalTheta))) - elemCenter(card[0])[1]
         const newX = containerCenter[0] + this.radius * (Math.cos(this.myTheta(card[1], globalTheta))) - elemCenter(card[0])[0]
@@ -84,7 +90,22 @@ class Circle {
       globalTheta += Math.PI / 360
       await sleep(5)
     }
+    // Store current theta as starting point for future rotation
     this.objects = this.objects.map(el => [el[0], this.myTheta(el[1], globalTheta)])
+  }
+
+  // Returns closest element to top of circle. This element will be the only one uncovered
+  // when wheel stops
+  getTopmostElement() {
+
+    // Map this.objects to array of 2-tuples containing element and absolute distance from pi/2 rads
+    const distancesFromTop = this.objects.map(el => {
+      return [el[0], Math.abs((Math.PI / 2) - (el[1] % (2 * Math.PI)))]
+    })
+    
+    // Sort array by distances from top and return first element
+    distancesFromTop.sort((a,b) => a[1] - b[1])
+    return distancesFromTop[0][0]
   }
 
 }
